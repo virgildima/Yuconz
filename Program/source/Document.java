ed9c0a94e961c8235325ddb45c05e26e48bd7e4a
Chris <cw509@kent.ac.uk>
2016-02-26 15:11:02 +0000

    Updated Document and PersonalDetails class.

--- Program/source/Document.java
+++ Program/source/Document.java
@@ -1,13 +1,4 @@
-import java.io.File;
-import java.io.IOException;
-
-import java.nio.file.Path;
-import java.nio.file.Files;
-import java.nio.file.attribute.BasicFileAttributes;
-
-import java.util.Date;
-import java.util.Set;
-import java.util.concurrent.TimeUnit;
+import java.util.HashMap;
 
 /**
  * Document class. 
@@ -16,53 +7,14 @@ import java.util.concurrent.TimeUnit;
  * @author Chris Wing 
  * @version 0.2
  */
-public abstract class Document extends DocumentAttributes implements DocumentInterface
-{
-    private static String stringPath = "C:\\Users\\Chris\\Desktop\\test.txt";
-    private static File filePath = new File("C:\\Users\\Chris\\Desktop\\test.txt");
-    private static Path path = filePath.toPath();
-    private static BasicFileAttributes attributes = null;
-        
-     /**
-     * public method setValue()
-     * 
-     * Method sets a new value to a documents attribute.
-     * 
-     * @param attributeName: The name of the attribute you wish to change the value of.
-     * @param newValue: The new value you wish to set attributeName as.
-     * 
-     * No meaningful return type. Either True or Void.
-     */
-    public void setValue(String attributeName, String newValue){
-        
-        switch(attributeName){
-        
-            case "name":
-                setName(newValue);
-                break;
-            case "age":
-                setAge(newValue);
-                break;
-            case "gender":
-                setGender(newValue);
-                break;
-            case "email_address":
-                setEmailAddress(newValue);
-                break;
-            case "mobile_number":
-                setMobileNumber(newValue);
-                break;
-            case "home_number":
-                setHomeNumber(newValue);
-                break;
-            case "home_address":
-                setHomeAddress(newValue);
-                break;
-            case "postcode":
-                setPostcode(newValue);
-                break;
-            default: break;
-        }
+public abstract class Document
+{  
+    private HashMap<String, String> documentAttributes = new HashMap<String, String>();
+            
+    
+    public HashMap returnMap(){
+    
+        return documentAttributes;
     }
     
      /**
@@ -72,64 +24,27 @@ public abstract class Document extends DocumentAttributes implements DocumentInt
      * 
      * Returns data in as a String.
      */
-    
-    @SuppressWarnings("deprecation")
-    public String getData(String dataType){
-        
-        if(dataType == "size"){
-            return String.valueOf(filePath.length());
-        }
-        if(dataType == "name"){
-                return filePath.getAbsolutePath().substring(filePath.getAbsolutePath().lastIndexOf("\\") + 1);
-        }
-        if (dataType == "created"){
-            
-            try{
-            
-                attributes = Files.readAttributes(path, BasicFileAttributes.class);
-            }
-            catch(IOException exception){
-            
-                return "Exception handled when trying to get file attributes: " + exception.getMessage();
-            }
-           
-            long milliseconds = attributes.creationTime().to(TimeUnit.MILLISECONDS);
-            if((milliseconds > Long.MIN_VALUE) && (milliseconds < Long.MAX_VALUE)){
-            
-                // Creates date based off of milliseconds.
-                Date creationDate = new Date(attributes.creationTime().to(TimeUnit.MILLISECONDS));
-               
-                return "Document: " + path.toString() + "created: " + creationDate.getDate() + "/" + 
-                        (creationDate.getMonth() + 1) + "/" + (creationDate.getYear() + 1900);
-            }
-        }
-        if(dataType == "type"){
-                return getExtension(filePath);
-        }
+    public String getData(String attributeName){
+   
+        if(attributeName != "" && attributeName != null && doesExist(attributeName) == true)
+            return documentAttributes.get(attributeName);
         else
-            throw new IllegalArgumentException("Invalid data type: " + dataType);
+            return "ERROR: whitespace not a valid source of input, or no valid attribute named: " + attributeName + ".";
     }
     
-     /**
-     * private method getExtension()
+    /**
+     * private method doesExist()
      * 
-     * Method used to get extension of active document/ file.
-     * 
-     * @param file: The name (more specifically the path) of the active file.
-     * 
-     * Returns String.
+     * Checks whether or not a String entered in exixts as a attribute within the 
+     * documentAttributes HashMap.
      */
-    private String getExtension(File file){
-    
-        // Gets file name.
-        String name = file.getName();
+    private boolean doesExist(String attribute){
         
-        try{
-            // Returns a string subset of which is before the last '.'.
-            return name.substring(name.lastIndexOf(".") + 1);
+        if(documentAttributes.containsKey(attribute) == true){
+        
+            return true;
         }
-        catch(Exception e){
-            return "";
-        }
+        else return false;
     }
+    
 }
