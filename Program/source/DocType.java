ebfb431f2c71f75dc8f51d61db84393065d4e3b4
apj8 <apj8@kent.ac.uk>
2016-03-24 18:16:09 +0000

    SaveToFile formatting on the Annual Review is fixed.

--- Program/source/DocType.java
+++ Program/source/DocType.java
@@ -93,101 +93,45 @@ public enum DocType
                 
                 writeParagraph(doc,ParagraphAlignment.CENTER, true,   18,"Annual Review.",true);
                 
-                writeParagraph(doc,ParagraphAlignment.CENTER, true,   18,"Staff No: "+  docC.getValue("staffID"),false);
-                writeParagraph(doc,ParagraphAlignment.CENTER, true,   18,"Name: "+ docC.getValue("name"),false);
+                writeParagraph(doc,ParagraphAlignment.CENTER, true,   18,"Staff No: "+  docC.getValue("staffID"),true);
+                writeParagraph(doc,ParagraphAlignment.CENTER, true,   18,"Name: "+ docC.getValue("name"),true);
                 writeParagraph(doc,ParagraphAlignment.LEFT,   false,  12,"Manager/Director: "+ docC.getValue("manager"),false);
                 writeParagraph(doc,ParagraphAlignment.LEFT,   false,  12,"Second Manager/Director: "+ docC.getValue("second_manager"),false);
                 writeParagraph(doc,ParagraphAlignment.LEFT,   false,  12,"Section: " + docC.getValue("section"),false);
-                writeParagraph(doc,ParagraphAlignment.LEFT,   false,  12,"Job Title: "+ docC.getValue("job_title"),false);
+                writeParagraph(doc,ParagraphAlignment.LEFT,   false,  12,"Job Title: "+ docC.getValue("job_title"),true);
                 
-                writeParagraph(doc,ParagraphAlignment.LEFT,   false,  12,"A review of the annual past performance: achievements and outcomes",false);
-                //create table
-                XWPFTable table = doc.createTable();
-                //create first row
-                XWPFTableRow tableRowOne = table.getRow(0);
-                table.setRowBandSize(1);
-                table.setWidth(1);
-                table.setColBandSize(1); 
-                table.setCellMargins(200, 200, 2500, 600);
-                tableRowOne.getCell(0).setText("No:");
-                tableRowOne.addNewTableCell().setText("Objectives");
-                tableRowOne.addNewTableCell().setText("Achievement: "+ docC.getValue("personal_goals_achieved"));
-                // Add a break between the tables
+                writeParagraph(doc,ParagraphAlignment.LEFT,   false,  14,"A review of the annual past performance: achievements and outcomes",false);
+                writeParagraph(doc,ParagraphAlignment.LEFT,   false,  12,docC.getValue("personal_goals_achieved"),true);
                 doc.createParagraph().createRun().addBreak();
-                //Create another table for performance sumary
-                XWPFTable table2 = doc.createTable();
-                //create first row
-                XWPFTableRow tableRowOne2 = table2.getRow(0);
-                table2.setRowBandSize(1);
-                table2.setWidth(1);
-                table2.setColBandSize(1); 
-                table2.setCellMargins(200, 200, 3500, 600);
-                tableRowOne2.getCell(0).setText("Performance summary: "+ docC.getValue("performance_summary"));
-                // Add a break between the tables
+                
+                writeParagraph(doc,ParagraphAlignment.LEFT,   false,  14,"Performance summary:",false);
+                writeParagraph(doc,ParagraphAlignment.LEFT,   false,  12,docC.getValue("performance_summary"),true);
                 doc.createParagraph().createRun().addBreak();
-                //paragraph for preview of future performances
-                XWPFParagraph tempParagraph10=doc.createParagraph();
-                tempParagraph10.setPageBreak(true);
-                tempParagraph10.setAlignment(ParagraphAlignment.LEFT);
-                XWPFRun tempRun10 = tempParagraph10.createRun();
-                tempRun10.setText("A preview of future performance:  goals/planned outcomes");
-                //create table
-                XWPFTable table10 = doc.createTable();
-                //create first row
-                XWPFTableRow tableRowOne10 = table10.getRow(0);
-                table10.setRowBandSize(1);
-                table10.setWidth(1);
-                table10.setColBandSize(1); 
-                table10.setCellMargins(200, 200, 2500, 600);
-                tableRowOne10.getCell(0).setText("No.");
-                tableRowOne10.addNewTableCell().setText(docC.getValue("future_goals_set"));
-                // Add a break between the tables
+                
+                writeParagraph(doc,ParagraphAlignment.LEFT,   false,  14,"A preview of future performance:  goals/planned outcomes",false);
+                writeParagraph(doc,ParagraphAlignment.LEFT,   false,  12,docC.getValue("future_goals_set"),true);
                 doc.createParagraph().createRun().addBreak();
-                //Create another table for reviewer comments
-                XWPFTable table4 = doc.createTable();
-                //create first row
-                XWPFTableRow tableRowOne4 = table4.getRow(0);
-                table4.setRowBandSize(1);
-                table4.setWidth(1);
-                table4.setColBandSize(1); 
-                table4.setCellMargins(200, 200, 3500, 600);
-                tableRowOne4.getCell(0).setText("Reviewer comments: " + docC.getValue("reviewer_comments"));
-                // Add a break between the tables
+                
+                writeParagraph(doc,ParagraphAlignment.LEFT,   false,  14,"Reviewer comments: ",false);
+                writeParagraph(doc,ParagraphAlignment.LEFT,   false,  12,docC.getValue("reviewer_comments"),true);
                 doc.createParagraph().createRun().addBreak();
-                //paragraph for preview of future Recommandation
-                writeParagraph(doc,ParagraphAlignment.LEFT,   false,  12,"Recommendation:  "+ docC.getValue("reviewer_recommendation"),false);
-    
-                //create table
-                XWPFTable table5 = doc.createTable();
-                //create first row
-                XWPFTableRow tableRowOne5 = table5.getRow(0);
-                table5.setRowBandSize(1);
-                table5.setWidth(1);
-                table5.setColBandSize(1);
-                table5.setCellMargins(100, 100, 800, 300);
-                tableRowOne5.getCell(0).setText("Reviewee signature  "+ docC.getValue("reviewee_signature"));
-                tableRowOne5.addNewTableCell().setText("                 ");
-                tableRowOne5.addNewTableCell().setText("Date  "+ docC.getValue("date_of_review"));
-                //create second row
-                XWPFTableRow tableRowOne6 = table5.createRow();
-                table5.setRowBandSize(1);
-                table5.setWidth(1);
-                table5.setColBandSize(1);
-                table5.setCellMargins(100, 100, 800, 300);
-                tableRowOne6.getCell(0).setText("Manager/Director signature     "+ docC.getValue("manager_signature"));
-                tableRowOne6.getCell(1).setText("                 ");
-                tableRowOne6.getCell(2).setText("Date  "+ docC.getValue("date_of_review"));
-    
-                //create third row
-                XWPFTableRow tableRowOne7 = table5.createRow();
-                table5.setRowBandSize(1);
-                table5.setWidth(1);
-                table5.setColBandSize(1);
-                table5.setCellMargins(100, 100, 800, 300);
-                tableRowOne7.getCell(0).setText("Second reviewer signature     "+ docC.getValue("second_manager_signatue"));
-                tableRowOne7.getCell(1).setText("                 ");
-                tableRowOne7.getCell(2).setText("Date  "+ docC.getValue("date_of_review"));
-    
+                
+                
+                writeParagraph(doc,ParagraphAlignment.LEFT,   false,  12,"Recommendation:  "+ docC.getValue("reviewer_recommendation"),true);
+                
+                
+                writeParagraph(doc,ParagraphAlignment.LEFT,   false,  14,"Reviewee signature",false);
+                writeParagraph(doc,ParagraphAlignment.LEFT,   false,  12,docC.getValue("reviewee_signature"),false);
+                writeParagraph(doc,ParagraphAlignment.LEFT,   false,  12,docC.getValue("date_of_reviewee_signature"),true);
+                
+                writeParagraph(doc,ParagraphAlignment.LEFT,   false,  14,"Reviewer signature",false);
+                writeParagraph(doc,ParagraphAlignment.LEFT,   false,  12,docC.getValue("manager_signature"),false);
+                writeParagraph(doc,ParagraphAlignment.LEFT,   false,  12,docC.getValue("date_of_manager_signature"),true);
+                
+                writeParagraph(doc,ParagraphAlignment.LEFT,   false,  14,"Second reviewer signature",false);
+                writeParagraph(doc,ParagraphAlignment.LEFT,   false,  12,docC.getValue("second_manager_signatue"),false);
+                writeParagraph(doc,ParagraphAlignment.LEFT,   false,  12,docC.getValue("date_of_second_manager_signature"),true);
+                
                 createDoc(filePath,doc);
             } catch (Exception e) {
                 return false;
@@ -202,11 +146,10 @@ public enum DocType
                 writeParagraph(doc,ParagraphAlignment.CENTER, true,   18,"Initial Employment.",true);
                 writeParagraph(doc,ParagraphAlignment.CENTER, true,   18,"Staff No: "+  docC.getValue("staffID"),true);
                 
-                writeParagraph(doc,ParagraphAlignment.LEFT,   false,  14,"The CV is located at: "+  docC.getValue("cv_filePath"),true);
+                writeParagraph(doc,ParagraphAlignment.LEFT,   false,  14,"The CV is located at: "+ new File(docC.getValue("cv_filePath")).getAbsolutePath(),true);
                 writeParagraph(doc,ParagraphAlignment.LEFT,   false,  14,"Interview Notes: "+ docC.getValue("interview_Notes"),true);
                 writeParagraph(doc,ParagraphAlignment.LEFT,   false,  14,"Interviewers: "+ docC.getValue("interviewers"),true);
                 writeParagraph(doc,ParagraphAlignment.LEFT,   false,  14,"Section: "+ docC.getValue("section"),false);
-                writeParagraph(doc,ParagraphAlignment.LEFT,   false,  14,"Role: "+ docC.getValue("role"),true);
                 writeParagraph(doc,ParagraphAlignment.LEFT,   false,  14,"Initial Job Title: "+ docC.getValue("initial_job_title"),false);
                 writeParagraph(doc,ParagraphAlignment.LEFT,   false,  14,"Initial Salary: "+ docC.getValue("initial_salary"),false);
                  writeParagraph(doc,ParagraphAlignment.LEFT,   false,  14,"Starting Date: "+ docC.getValue("start_date"),false);
@@ -252,7 +195,7 @@ public enum DocType
                 writeParagraph(doc,ParagraphAlignment.CENTER, true,   18,"Probation Document.",true);
                 writeParagraph(doc,ParagraphAlignment.CENTER, true,   18,"Staff No: "+  docC.getValue("staffID"),true);
                 
-                writeParagraph(doc,ParagraphAlignment.LEFT,   false,  14,"Surname: "+  docC.getValue("surname"),true);
+                writeParagraph(doc,ParagraphAlignment.LEFT,   false,  14,"Surname: "+  docC.getValue("surname"),false);
                 writeParagraph(doc,ParagraphAlignment.LEFT,   false,  14,"First Name: "+ docC.getValue("firstname"),true);
                 writeParagraph(doc,ParagraphAlignment.LEFT,   false,  14,"Probation reason: "+ docC.getValue("probation_reason"),true);
                 writeParagraph(doc,ParagraphAlignment.LEFT,   false,  14,"Probation Start Date: "+ docC.getValue("probation_start_date"),false);
@@ -273,12 +216,12 @@ public enum DocType
                 writeParagraph(doc,ParagraphAlignment.CENTER, true,   18,"Promotion Document.",true);
                 writeParagraph(doc,ParagraphAlignment.CENTER, true,   18,"Staff No: "+  docC.getValue("staffID"),true);
                 
-                writeParagraph(doc,ParagraphAlignment.LEFT,   false,  14,"Surname: "+  docC.getValue("surname"),true);
+                writeParagraph(doc,ParagraphAlignment.LEFT,   false,  14,"Surname: "+  docC.getValue("surname"),false);
                 writeParagraph(doc,ParagraphAlignment.LEFT,   false,  14,"First Name: "+ docC.getValue("firstname"),true);
-                writeParagraph(doc,ParagraphAlignment.LEFT,   false,  14,"Current Job Title: "+ docC.getValue("current_job_title"),true);
-                writeParagraph(doc,ParagraphAlignment.LEFT,   false,  14,"Current Section: "+ docC.getValue("current_section"),false);
-                writeParagraph(doc,ParagraphAlignment.LEFT,   false,  14,"New Job Title: "+ docC.getValue("new_job_title"),true);
-                writeParagraph(doc,ParagraphAlignment.LEFT,   false,  14,"New Job Section: "+ docC.getValue("new_job_section"),false);
+                writeParagraph(doc,ParagraphAlignment.LEFT,   false,  14,"Current Job Title: "+ docC.getValue("current_job_title"),false);
+                writeParagraph(doc,ParagraphAlignment.LEFT,   false,  14,"Current Section: "+ docC.getValue("current_section"),true);
+                writeParagraph(doc,ParagraphAlignment.LEFT,   false,  14,"New Job Title: "+ docC.getValue("new_job_title"),false);
+                writeParagraph(doc,ParagraphAlignment.LEFT,   false,  14,"New Job Section: "+ docC.getValue("new_job_section"),true);
                 writeParagraph(doc,ParagraphAlignment.LEFT,   false,  14,"Starting Date: "+ docC.getValue("starting_date"),false);
                 
                 createDoc(filePath,doc);
